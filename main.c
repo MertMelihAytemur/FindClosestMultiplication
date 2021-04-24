dc59d96665a606b9eaec0bded46d5db35d59a04d
MertMelihAytemur <55717182+MertMelihAytemur@users.noreply.github.com>
2021-04-24 10:42:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
 
 void setMultiplicationIndexesLowerDistanceToTarget(const int array[], int target, int array_size, int *first_index, int *second_index);
 
 #define SIZE_OF_ARRAY 7
 
 int main() {
-    int numbers_to_check[SIZE_OF_ARRAY] = {-1,2,-3,4,-5,-6,7}; // 2,3,4,6,8,12
-    int target = 30;
+    int numbers_to_check[SIZE_OF_ARRAY] = {-1,-2,-3,-4,-5,-6,-7}; // 2,3,4,6,8,12
+    int target = -30;
     int first_index, second_index;
 
     setMultiplicationIndexesLowerDistanceToTarget(numbers_to_check, target, SIZE_OF_ARRAY, &first_index, &second_index);
@@ -20,21 +21,16 @@ void setMultiplicationIndexesLowerDistanceToTarget(
         int array_size,
         int *first_index,
         int *second_index) {
-    int multiplication, currentMultiplication = 0;
+    int multiplication;
+    int diff = INT_MAX;
     for (int i = 0; i < array_size; i++) {
         for (int j = i + 1; j < array_size; j++) {
             multiplication = array[i] * array[j];
-            if (target > 0) {
-                if (abs(target - multiplication) < abs(target - currentMultiplication)) {
-                    *first_index = i;
-                    *second_index = j;
-                    currentMultiplication = multiplication;
-                }
-            } else if (target < 0) {
-                //TODO Burayı Doldur.
+            if (abs(target - multiplication) < diff) {
+                diff = abs(target - multiplication);
+                *first_index = i;
+                *second_index = j;
             }
         }
     }
 }
-
-
